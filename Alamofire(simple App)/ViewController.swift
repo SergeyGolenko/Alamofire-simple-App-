d21dd02129da1404501b3ba2f758efd097bc6e4c
SergeyGolenko <1989sergiotay@gmail.com>
2020-02-26 17:56:48 +0200

    Initial Commit

---
+++ Alamofire(simple App)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alamofire(simple App)
+//
+//  Created by Sergey Golenko on 26.02.2020.
+//  Copyright © 2020 Sergey Golenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
