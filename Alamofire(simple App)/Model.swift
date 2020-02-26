06f68c53ed3de367b79abd6255d0c59dcef3fce6
SergeyGolenko <1989sergiotay@gmail.com>
2020-02-26 18:42:05 +0200

    i add new file for model Item

---
+++ Alamofire(simple App)/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  Alamofire(simple App)
+//
+//  Created by Sergey Golenko on 26.02.2020.
+//  Copyright © 2020 Sergey Golenko. All rights reserved.
+//
+
+import Foundation
+
+struct Item:Codable{
+    var albumID:Int
+    var id:Int
+    
+}
