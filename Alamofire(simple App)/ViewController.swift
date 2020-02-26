f5577ad720a2f7c47dbe692c6f773308fc0ca2d4
SergeyGolenko <1989sergiotay@gmail.com>
2020-02-26 18:07:24 +0200

    add print in the VC

--- Alamofire(simple App)/ViewController.swift
+++ Alamofire(simple App)/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("This is VC")
         // Do any additional setup after loading the view.
     }
 
