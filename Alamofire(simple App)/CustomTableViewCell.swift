87b112000407cad35d5bee9b2575348e619daa80
SergeyGolenko <1989sergiotay@gmail.com>
2020-02-26 19:16:31 +0200

    add viewCellVC

---
+++ Alamofire(simple App)/CustomTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTableViewCell.swift
+//  Alamofire(simple App)
+//
+//  Created by Sergey Golenko on 26.02.2020.
+//  Copyright © 2020 Sergey Golenko. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
